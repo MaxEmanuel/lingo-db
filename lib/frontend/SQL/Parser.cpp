1cc9d20ab8d0fa47f31243064facf3442199b165
Kevin Gutjahr <kevin.gutjahr@stud.uni-bamberg.de>
2024-11-29 01:13:00 +0100

    translate new TABLE expressions

--- lib/frontend/SQL/Parser.cpp
+++ lib/frontend/SQL/Parser.cpp
@@ -1905,6 +1905,10 @@ mlir::Value frontend::sql::Parser::translateExpression(mlir::OpBuilder& builder,
                mlir::Value exists = builder.create<mlir::relalg::ExistsOp>(loc, builder.getI1Type(), subQueryTree);
                return builder.create<mlir::db::NotOp>(loc, exists);
             }
+            case TABLE_SUBLINK: {
+               assert(!targetInfo.namedResults.empty());
+               return subQueryTree;
+            }
             default:
               throw std::runtime_error("unsupported sublink type");
          }
@@ -1937,6 +1941,10 @@ mlir::Value frontend::sql::Parser::translateExpression(mlir::OpBuilder& builder,
       case T_RowExpr: {
          mlir::Value rowRel = translateRowExpression(builder, context, node);
          return rowRel;
+      }
+      case T_RangeVar: {
+         auto scope = context.createResolverScope();
+         return translateRangeVar(builder, reinterpret_cast<RangeVar*>(node), context, scope);
       }
       default: {
         throw std::runtime_error("unsupported expression type");
