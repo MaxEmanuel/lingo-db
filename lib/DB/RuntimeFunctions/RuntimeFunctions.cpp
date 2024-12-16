eff8ef4fc2e3d84621fc9da90f2c1788f2a84366
Kevin Gutjahr <kevin.gutjahr@stud.uni-bamberg.de>
2024-12-16 22:50:57 +0100

    remove old AutoDiff.cpp file

--- lib/DB/RuntimeFunctions/RuntimeFunctions.cpp
+++ lib/DB/RuntimeFunctions/RuntimeFunctions.cpp
@@ -8,7 +8,6 @@
 #include "runtime-defs/IntegerRuntime.h"
 #include "runtime-defs/StringRuntime.h"
 #include "runtime-defs/ArrayRuntime.h"
-#include "runtime-defs/AutoDiff.h"
 #include "runtime-defs/Timing.h"
 #include "runtime/DateRuntime.h"
 
@@ -360,7 +359,5 @@ std::shared_ptr<mlir::db::RuntimeFunctionRegistry> mlir::db::RuntimeFunctionRegi
    builtinRegistry->add("ArrayNullCheck").implementedAs(rt::ArrayRuntime::isNull).matchesTypes({RuntimeFunction::arrayLike}, resTypeIsBool);
 
    builtinRegistry->add("ArrayAgg").implementedAs(rt::ArrayRuntime::agg).matchesTypes({RuntimeFunction::arrayLike, RuntimeFunction::arrayLike, RuntimeFunction::intLike, RuntimeFunction::intLike}, resTypeIsAnyArray);
-   builtinRegistry->add("AutoDiff").matchesTypes({RuntimeFunction::intLike}, RuntimeFunction::matchesArgument()).implementedAs(rt::AutoDiff::derivate);
-
    return builtinRegistry;
 }
--- lib/runtime/AutoDiff.cpp
+++
@@ -1,8 +0,0 @@
-#include "runtime/helpers.h"
-#include "runtime/AutoDiff.h"
-#include <iostream>
-#include <string>
-
-int32_t runtime::AutoDiff::derivate(int32_t varVal) {
-    return varVal;
-}
