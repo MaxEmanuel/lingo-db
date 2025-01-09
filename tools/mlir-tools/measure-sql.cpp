bf8c25d0cc4e8e92f412df78c7e5df996b746f97
Kevin Gutjahr <kevin.gutjahr@stud.uni-bamberg.de>
2025-01-09 16:11:36 +0100

    save summarized data

--- tools/mlir-tools/measure-sql.cpp
+++ tools/mlir-tools/measure-sql.cpp
@@ -1,6 +1,6 @@
 #include <fstream>
 #include <iostream>
-#include <filesystem>
+#include <algorithm>
 #include <string>
 
 #include "execution/Execution.h"
@@ -13,24 +13,24 @@ void writeToCSV(std::unordered_map<std::string, double>& data, std::string fileP
 std::unordered_map<std::string, std::vector<double>> readCSV(std::string filePath);
 double calcMean(std::vector<double> data);
 double calcStdDev(std::vector<double> data);
+std::vector<double> calcCompilationTime(std::vector<std::unordered_map<std::string, double>> dataVec);
 
 int main(int argc, char** argv) {
     if (argc <= 4) {
-       std::cerr << "USAGE: run-sql *.sql database number_of_repetitions *.csv" << std::endl;
+       std::cerr << "USAGE: run-sql *.sql database number_of_repetitions [base file name]" << std::endl;
        return 1;
     }
+    // arguments
     std::string inputFileName = std::string(argv[1]);
     std::string directory = std::string(argv[2]);
     size_t repetitions = std::atoi(argv[3]);
-    std::filesystem::path timingFile = std::string(argv[4]);
+    std::string baseFileName = std::string(argv[4]);
 
     // file paths
-    std::filesystem::path folder = timingFile.parent_path();
-    std::string fileNameNoExt = timingFile.stem().string();
-    std::string filePathNoExt = (folder / fileNameNoExt).string();
-    std::string timingFileExtension = timingFile.extension().string();
-    std::string timingRawDataFile = filePathNoExt + "_raw" + timingFileExtension;
-    std::string timingProcessedDataFile = filePathNoExt + "_processed.csv" + timingFileExtension;
+    std::string timeRawFile = baseFileName + "_raw.csv";
+    std::string timeProcessedFile = baseFileName + "_processed.csv";
+    std::string compTimeRawFile = baseFileName + "Summarized_raw.csv";
+    std::string compTimeProcessedFile = baseFileName + "Summarized_processed.csv";
 
     // loading database
     std::cout << "Loading Database from: " << directory << '\n';
@@ -41,6 +41,7 @@ int main(int argc, char** argv) {
     std::ofstream file("/dev/null");
     std::streambuf *buffer = std::cout.rdbuf();
     std::cout.rdbuf(file.rdbuf());
+    std::vector<std::unordered_map<std::string, double>> times;
 
     // running the measurements
     for(size_t i = 0; i < repetitions; i++) {
@@ -61,12 +62,13 @@ int main(int argc, char** argv) {
 
         // write time data to raw data file
         auto timing = rawPtr->timingProcessor->getTimeMap();
-        writeToCSV(timing, timingRawDataFile);
+        times.push_back(timing);
+        writeToCSV(timing, timeRawFile);
     }
     std::cout.rdbuf(buffer);
 
     // process data, i.e. calculate mean and standard deviation.
-    auto csv = readCSV(timingRawDataFile);
+    auto csv = readCSV(timeRawFile);
     std::unordered_map<std::string, double> processedTiming;
     for (auto [name, t] : csv) {
         std::string nameMean = name + "_mean";
@@ -76,7 +78,35 @@ int main(int argc, char** argv) {
     }
 
     // Store the values in new file
-    writeToCSV(processedTiming, timingProcessedDataFile);
+    writeToCSV(processedTiming, timeProcessedFile);
+
+    std::unordered_map<std::string, std::vector<double>> comp;
+    comp["execution"] = csv["executionTime"];
+    comp["optimization"] = csv["QOpt"];
+    comp["compilation"] = calcCompilationTime(times);
+
+    std::vector<std::unordered_map<std::string, double>> result(comp.begin()->second.size());
+    for (const auto& [key, vec] : comp) {
+        for (size_t i = 0; i < vec.size(); ++i) {
+            result[i][key] = vec[i];
+        }
+    }
+
+    for(auto row : result) {
+        writeToCSV(row, compTimeRawFile);
+    }
+
+    std::unordered_map<std::string, double> processedCompTiming;
+    for (auto [name, t] : comp) {
+        std::string nameMean = name + "_mean";
+        std::string namestdDev = name + "_stdDev";
+        processedCompTiming[nameMean] = calcMean(t);
+        processedCompTiming[namestdDev] = calcStdDev(t);
+    }
+
+    // Store the values in new file
+    writeToCSV(processedCompTiming, compTimeProcessedFile);
+    
     return 0;
 }
 
@@ -130,6 +160,7 @@ std::unordered_map<std::string, std::vector<double>> readCSV(std::string filePat
 
     // create a emtpy vector for each header
     while (std::getline(headerStream, headerCell, ',')) {
+        headerCell = headerCell.substr(headerCell.find_first_not_of(' '), headerCell.find_last_not_of(' ') - headerCell.find_first_not_of(' ') + 1);
         csvData[headerCell] = std::vector<double>();
         headerList.push_back(headerCell);
     }
@@ -169,4 +200,17 @@ double calcStdDev(std::vector<double> data) {
     }
     double stdDev = std::sqrt(deviation / (data.size() - 1));
     return stdDev;
+}
+
+std::vector<double> calcCompilationTime(std::vector<std::unordered_map<std::string, double>> dataVec) {
+    std::vector<std::string> compParts = {"lowerRelAlg", "lowerSubOp", "lowerDB", "lowerDSA", "lowerToLLVM", "toLLVMIR", "llvmOptimize", "llvmCodeGen"};
+    std::vector<double> compTime(dataVec.size(), 0.0);
+    for(size_t i = 0; i < dataVec.size(); i++) {
+        for(auto [name, time] : dataVec[i]) {
+            if(std::find(compParts.begin(), compParts.end(), name) != compParts.end()) {
+                compTime[i] += time;
+            }
+        }
+    }
+    return compTime;
 }
